62531e560759fea521d938061732031aa74f160a
Christian Roque <christianmatias@outlook.com>
2023-07-01 15:07:47 -0300

    aula 26

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+;
+function ola(nome) {
+    return nome;
+}
+;
+console.log(multiplicacao(2, 3));
+console.log(ola(`Olá ${'Christian'}!`));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+};
+
+function ola(nome:string): string {
+    return nome;
+};
+
+console.log(multiplicacao(2, 3));
+console.log(ola(`Olá ${'Christian'}!`));
